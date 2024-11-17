1762061d98bfe849357e6a1a7e8b874327a2614b
Arthur Dufour <dufourarthur.perso@gmail.com>
2024-11-17 20:26:50 +0100

    feat: improve project page layout

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -9,9 +9,23 @@ useSeoMeta({
   <main class="min-h-screen">
     <div class="prose dark:prose-invert prose-blockquote:not-italic prose-pre:bg-gray-900 prose-img:rounded-lg prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10">
       <ContentDoc v-slot="{ doc }" tag="div">
-        <h1>
-          {{ doc.name }}
-        </h1>
+        <div class="flex justify-between items-center gap-2">
+          <div class="flex items-center gap-2">
+            <h1 class="mb-0 text-3xl">
+              {{ doc.name }}
+            </h1>
+          </div>
+
+          <div class="flex items-center gap-2">
+            <NuxtLink v-if="doc.github" :to="doc.github" external>
+              <Icon name="logos:github-icon" />
+            </NuxtLink>
+
+            <NuxtLink v-if="doc.url" :to="doc.url" external class="text-sm">
+              {{ doc.url }}
+            </NuxtLink>
+          </div>
+        </div>
 
         <ContentRenderer :value="doc" />
       </ContentDoc>
